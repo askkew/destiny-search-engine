cf03ff83a49f391c14b9c13d1fa8eb49abd58ea0
Lucas Overbey <lucasaoverbey@gmail.com>
2023-07-07 20:59:42 -0500

    update to endpoint

--- server/server.js
+++ server/server.js
@@ -12,15 +12,71 @@ const apiKey = process.env.X_API_KEY;
 
 //-------endpoints--------
 
+// app.get('/search', async (req, res) => {
+//   const { searchQuery } = req.query;
+//   let page = 0;
+//   let hasMoreResults = true;
+//   const results = [];
+
+//   try {
+//     while (hasMoreResults && results.length < 100) {
+//       const response = await axios.get(`https://www.bungie.net/Platform/Destiny2/Armory/Search/DestinyInventoryItemDefinition/${searchQuery}/?page=${page}`, {
+//         headers: {
+//           'X-API-Key': apiKey,
+//         }
+//       });
+
+//       const currentResults = response.data.Response.results.results;
+//       results.push(...currentResults);
+//       page++;
+
+//       if (currentResults.length < 25) {
+//         hasMoreResults = false;
+//       }
+//     }
+
+//     const resultWithAdditionalData = await Promise.all(
+//       results.map(async (result) => {
+//         const hash = result.hash;
+//         const additionalDataResponse = await axios.get(
+//           `https://www.bungie.net/Platform/Destiny2/Manifest/DestinyInventoryItemDefinition/${hash}`,
+//           {
+//             headers: {
+//               'X-API-Key': apiKey,
+//             },
+//           }
+//         );
+//         const additionalData = additionalDataResponse.data.Response;
+//         return { ...result, additionalData };
+//       })
+//     );
+
+//     res.json({ Response: { results: resultWithAdditionalData } });
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).send('Server Error');
+//   }
+// });
+
+
 app.get('/search', async (req, res) => {
-  const { searchQuery, page } = req.query;
+  const { searchQuery } = req.query;
+  let page = 0;
+  const results = [];
+
   try {
-    const response = await axios.get(`https://www.bungie.net/Platform/Destiny2/Armory/Search/DestinyInventoryItemDefinition/${searchQuery}/?page=${page}`, {
-      headers: {
-        'X-API-Key': apiKey,
-      }
-    });
-    const results = response.data.Response.results.results;
+    let response; // Declare response variable
+
+    do {
+      response = await axios.get(`https://www.bungie.net/Platform/Destiny2/Armory/Search/DestinyInventoryItemDefinition/${searchQuery}/?page=${page}`, {
+        headers: {
+          'X-API-Key': apiKey,
+        }
+      });
+      results.push(...response.data.Response.results.results);
+      page++;
+    } while (response.data.Response.results.results.length >= 25 && results.length < 100);
+
     const resultWithAdditionalData = await Promise.all(
       results.map(async (result) => {
         const hash = result.hash;
@@ -43,22 +99,22 @@ app.get('/search', async (req, res) => {
   }
 });
 
+
+const PORT = 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+
+
+
 // app.get('/search', async (req, res) => {
-//   const { searchQuery } = req.query;
-//   let page = 0;
-//   const results = [];
-
+//   const { searchQuery, page } = req.query;
 //   try {
-//     do {
-//       const response = await axios.get(`https://www.bungie.net/Platform/Destiny2/Armory/Search/DestinyInventoryItemDefinition/${searchQuery}/?page=${page}`, {
-//         headers: {
-//           'X-API-Key': apiKey,
-//         }
-//       });
-//       results.push(...response.data.Response.results.results);
-//       page++;
-//     } while (response.data.Response.results.results.length >= 25);
-
+//     const response = await axios.get(`https://www.bungie.net/Platform/Destiny2/Armory/Search/DestinyInventoryItemDefinition/${searchQuery}/?page=${page}`, {
+//       headers: {
+//         'X-API-Key': apiKey,
+//       }
+//     });
+//     const results = response.data.Response.results.results;
 //     const resultWithAdditionalData = await Promise.all(
 //       results.map(async (result) => {
 //         const hash = result.hash;
@@ -80,9 +136,3 @@ app.get('/search', async (req, res) => {
 //     res.status(500).send('Server Error');
 //   }
 // });
-
-//
-
-const PORT = 5000;
-
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
